6cdc435e524a2159d02c4f1276a97752e8a6a078
Pablo <pablo.um00@gmail.com>
2018-10-17 15:40:25 -0500

    Create new user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,24 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      @errors = @user.errors.full_messages
+      render 'users/new'
+    end
+  end
+
+
+
+  def user_params
+    params.require(:user).permit(:name)
+  end
 end
